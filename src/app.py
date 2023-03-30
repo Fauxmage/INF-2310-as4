c95b6810829e45e98f23e67cb0fbe239e27008d6
Ulrik Johnsen <ulrikjohnsen@tromso-eksterne-1435.wifi.uit.no>
2023-03-30 22:32:24 +0200

    Templates and css

--- src/app.py
+++ src/app.py
@@ -9,6 +9,16 @@ def home():
 
     return render_template('index.html')
 
+@app.route("/register", methods=['GET'])
+def register_get():
+
+    return render_template('register.html')
+
+@app.route("/register", methods=['POST'])
+def register_post():
+
+    return render_template('register.html')
+
 
 if __name__ == "__main__":
     app.run() 
