32bcdbd9150de8712be3819732ce686fe8fb6c3e
Ulrik Johnsen <ulrikjohnsen@Ulriks-MacBook-Pro.local>
2023-04-06 17:42:10 +0200

    added docs, finished a few views, login and registration works, however still working on authentication check, and need to add cryptography, this is just bare minimum, and a little less

---
+++ docs/README.md
@@ -0,0 +1,6 @@
+# Instructions on running the server
+
+1. Open your terminal and navigate to the directory containing the /src and enter it.
+2. In the terminal window, enter the following command "python app.py", or "python3 app.py" if just "python" does not run. 
+3. Open your web browser and go to "localhost:5000", navigate the website, register a user, and login. 
+
--- src/app.py
+++ src/app.py
@@ -3,21 +3,75 @@ import os.path
 
 app = Flask(__name__, template_folder='templates', static_folder='styles')
 
+USER_CREDENTIALS = "user_creds.txt"
 
 @app.route("/")
 def home():
-
+    
+    # Returns homepage/index page
     return render_template('index.html')
 
-@app.route("/register", methods=['GET'])
+@app.route("/authenticated", methods=['GET'])
 def register_get():
+    """
+    Authentication check for users. 
+    If the user is logged in, then the authentication check approves.
+    If the user is not logged in, the authentication check is not approved.
+    """
 
-    return render_template('register.html')
+    #username = request.form['username']
+    #password = request.form['password']
+    
+    
 
-@app.route("/register", methods=['POST'])
+    return render_template('authenticated.html')
+
+@app.route("/", methods=['POST'])
 def register_post():
+    
+    # Get the user credentials
+    username = request.form['username']
+    password = request.form['password']
+    
+    
+    # TO-DO: Check if user already exists
+    
 
-    return render_template('register.html')
+    # Write the user credentials to the file
+    with open(USER_CREDENTIALS, 'a') as f:
+        f.write(f"{username},{password}\n")
+        #return "User registration successfull!"
+        return render_template('loggedin.html')
+    
+
+@app.route("/login", methods=['POST', 'GET'])
+def log_in():
+    
+    if request.method == 'POST':
+        # User credentials
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the user exists in our file
+        with open(USER_CREDENTIALS, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                if username in line:
+
+                    # Check whether the password exists for the specific user
+                    exists_username, exists_password = line.strip().split(',')
+                    if exists_password == password:
+                        #return f"Welcome, {username}!"
+                        return render_template('loggedin.html')
+
+                    # If not exist, return invalid credentials message
+                    else:
+                        return "Invalid credentials"
+
+    # If GET request, return the login template
+    else:
+        return render_template('login.html')
+        
 
 
 if __name__ == "__main__":
